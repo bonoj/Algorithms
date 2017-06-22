2e9f332cb560d3d2fda69605255f5e8ed0c8ecde
bonoj <johnlbono@gmail.com>
2017-06-22 08:43:18 -0500

    Eulerian tour.

--- Algorithms.py
+++ Algorithms.py
@@ -7,20 +7,16 @@ def russian(a, b):
     x = a; y = b
     z = 0
     while x > 0:
+        if x == 7 and z == 84:
+            print(y)
         if x % 2 == 1: z = z + y
         y = y << 1
         x = x >> 1
     return z
 
-# print(russian(20, 7))
+# print(russian(63, 12))
 
 # print("--- %s seconds ---" % (time.time() - start_time))
-
-# print("--- %s seconds ---" % "John")
-
-# start_time = "john"
-
-# print(start_time)
 
 
 def naive(a,b):
@@ -31,11 +27,37 @@ def naive(a,b):
         x = x - 1
     return z
 
-print(naive(20, 12))
+# print(naive(20, 12))
 
 def rec_naive(a, b):
     if a == 0:
         return 0
     return b + rec_naive(a-1, b)
 
-print( rec_naive(17,6))
+# print( rec_naive(17,6))
+
+# Find Eulerian Tour
+#
+# Write a function that takes in a graph
+# represented as a list of tuples
+# and return a list of nodes that
+# you would follow on an Eulerian Tour
+#
+# For example, if the input graph was
+# [(1, 2), (2, 3), (3, 1)]
+# A possible Eulerian tour would be [1, 2, 3, 1]
+
+def find_eulerian_tour(graph):
+    # your code here
+    tour = []
+    l = len(graph)
+    for i in range(l):
+        t = edge(graph[i], graph[(i + 1) % 1])
+        tour.append(t)
+    return tour
+
+
+def edge(x, y):
+    return (x, y) if x < y else (y, x)
+
+print(find_eulerian_tour([(1, 2), (2, 3), (3, 1)]))
