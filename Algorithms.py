74441122a8621814bc3affba8cc0cf444b1fea0d
bonoj <johnlbono@gmail.com>
2017-06-22 10:24:13 -0500

    grid networks

--- Algorithms.py
+++ Algorithms.py
@@ -60,4 +60,37 @@ def find_eulerian_tour(graph):
 def edge(x, y):
     return (x, y) if x < y else (y, x)
 
-print(find_eulerian_tour([(1, 2), (2, 3), (3, 1)]))
+# print(find_eulerian_tour([(1, 2), (2, 3), (3, 1)]))
+
+
+# sqrt = 256**(1/2.0)
+# print(sqrt)
+
+x = 15 * 16
+print(2 * x)
+
+def make_link(G, node1, node2):
+    if node1 not in G:
+        G[node1] = {}
+    (G[node1])[node2] = 1
+    if node2 not in G:
+        G[node2] = {}
+    (G[node2])[node1] = 1
+    return G
+
+G = {}
+
+n = 256
+side = int(math.sqrt(n))
+
+# Add in the edges
+for i in range(side):
+    for j in range(side):
+        if i < side-1: make_link(G, (i, j), (i + 1, j))
+        if j < side - 1: make_link(G, (i, j), (i, j + 1))
+
+# How many nodes?
+print(len(G))
+
+# How many edges?
+print(sum([len(G[node]) for node in G.keys()])/2)
