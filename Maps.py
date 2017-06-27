e8ab3baeef7c83e72e9ed3837534a08abb2e54cc
bonoj <johnlbono@gmail.com>
2017-06-27 15:16:04 -0500

    Sets, Maps, Dictionaries.

---
+++ Maps.py
@@ -0,0 +1,13 @@
+# Dictionaries!
+
+udacity = {}
+udacity['u'] = 1
+udacity['d'] = 2
+udacity['a'] = 3
+udacity['c'] = 4
+udacity['i'] = 5
+udacity['t'] = 6
+udacity['y'] = 7
+
+print(udacity)
+# {'u': 1, 'd': 2, 'a': 3, 'c': 4, 'i': 5, 't': 6, 'y': 7}
