49e4e3da98afdbf017fbddd78c532a5a5877b0e9
bonoj <johnlbono@gmail.com>
2017-06-27 14:34:13 -0500

    Not happy with Quicksort.

--- SearchingAndSorting.py
+++ SearchingAndSorting.py
@@ -120,3 +120,44 @@ def binary_search(input_array, value):
 # test_val2 = 15
 # print(recursive_binary_search(test_list, test_val1))
 # print(recursive_binary_search(test_list, test_val2))
+
+# """Implement quick sort in Python.
+# Input a list.
+# Output a sorted list."""
+
+# def quicksort(array):
+#     print(array)
+#
+#     pivot = array[len(array) - 1]
+#     if array[0] > pivot:
+#         array[len(array) - 1] = array[0]
+#         array[0] = array[len(array) - 2]
+#         array[len(array) - 2] = pivot
+#
+#     print(array)
+#     return []
+
+def quicksort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return quicksort(less) + equal + quicksort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to hande the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(quicksort(test))
+
+
